d25f9bf92831ba510aabecabd1f6af64a85e2953
Walter Boring <waboring@hemna.com>
2025-11-01 21:00:25 -0400

    First stab at the GPSBeaconThread

--- aprsd_gps_extension/conf/main.py
+++ aprsd_gps_extension/conf/main.py
@@ -1,6 +1,5 @@
 from oslo_config import cfg
 
-
 extension_group = cfg.OptGroup(
     name="aprsd_gps_extension",
     title="APRSD gps extension settings",
@@ -11,6 +10,44 @@ extension_opts = [
         "enabled",
         default=True,
         help="Enable the extension?",
+    ),
+    cfg.StrOpt(
+        "gpsd_host",
+        default="localhost",
+        help="GPSD host to connect to. Ensure gpsd is running and listening on this host.",
+    ),
+    cfg.IntOpt(
+        "gpsd_port",
+        default=2947,
+        help="GPSD port to connect to",
+    ),
+    cfg.IntOpt(
+        "polling period",
+        default=10,
+        help="Polling period in seconds to get the GPS data",
+    ),
+    cfg.BoolOpt(
+        "debug",
+        default=False,
+        help="Enable debug logging",
+    ),
+    cfg.BoolOpt(
+        "enable_smart_beacon",
+        default=False,
+        help="Enable the smart beacon feature. If this is disabled, the beacon will be sent every"
+        "CONF.beacon_interval seconds. When enabled, the beacon will be sent only if the"
+        "device has moved a certain distance or time since the last beacon was sent.",
+    ),
+    cfg.IntOpt(
+        "smart_beacon_distance_threshold",
+        default=100,
+        help="The distance in feet that the device must move before sending a beacon packet,"
+        "when smart beaconing is enabled.",
+    ),
+    cfg.IntOpt(
+        "smart_beacon_time_window",
+        default=10,
+        help="The time window in minutes that the device must be at the same position before sending a beacon packet, when smart beaconing is enabled.",
     ),
 ]
 
---
+++ aprsd_gps_extension/threads/GPSBeaconThread.py
@@ -0,0 +1,136 @@
+import logging
+import time
+
+from aprsd import threads as aprsd_threads
+from aprsd.packets import core
+from aprsd.threads import tx as aprsd_tx
+from gpsdclient import GPSDClient
+from oslo_config import cfg
+
+from aprsd_gps_extension.utils import beacon_processor
+
+CONF = cfg.CONF
+LOG = logging.getLogger("APRSD")
+
+
+class GPSBeaconThread(aprsd_threads.APRSDThread):
+    """Thread the reads the GPS data from gpsdclient and sends a beacon packet
+    based on either smart beaconing or regular beaconing.
+
+    regular beaconing is when the beacon is sent every CONF.beacon_interval seconds.
+    smart beaconing is when the beacon is sent only if the device has moved a certain
+    distance or time since the last beacon was sent.
+    """
+
+    def __init__(self):
+        super().__init__("GPSClientThread")
+        self.client = None
+        self.setup()
+        # Now setup the initial timer
+        self.last_beacon_time = time.time()
+
+    def setup(self):
+        LOG.info("GPSClientThread setup")
+        if not CONF.enable_beacon:
+            LOG.error("Beaconing is disabled, stopping thread.")
+            LOG.error(
+                "Enable beaconing in the config file with CONF.enable_beacon = True."
+            )
+            self.stop()
+            return False
+
+        if not CONF.aprsd_gps_extension.gpsd_host:
+            LOG.error("GPSD host not set")
+            return
+
+        try:
+            LOG.info(
+                f"Connecting to GPS daemon: {CONF.aprsd_gps_extension.gpsd_host}:{CONF.aprsd_gps_extension.gpsd_port}"
+            )
+            self.client = GPSDClient(
+                host=CONF.aprsd_gps_extension.gpsd_host,
+                port=CONF.aprsd_gps_extension.gpsd_port,
+            )
+            LOG.info("Connected to GPS daemon")
+        except Exception as e:
+            LOG.error(f"Error connecting to GPS daemon: {e}")
+            return
+
+    def _debug(self, message):
+        if CONF.aprsd_gps_extension.debug:
+            LOG.debug(f"GPS: {message}")
+
+    def send_beacon(self, tpv_data, sky_data):
+        LOG.info("Sending beacon")
+        pkt = core.BeaconPacket(
+            from_call=CONF.callsign,
+            to_call="APRS",
+            latitude=tpv_data.get("lat"),
+            longitude=tpv_data.get("lon"),
+            comment="APRSD GPS Beacon",
+            symbol=CONF.beacon_symbol,
+        )
+        aprsd_tx.send(pkt, direct=True)
+        self.last_beacon_time = time.time()
+
+    def loop(self):
+        LOG.info("GPSClientThread loop")
+        if self.loop_count % CONF.aprsd_gps_extension.polling_period == 0:
+            # Collect the latest TPV and SKY messages from the stream
+            tpv_data = None
+            sky_data = None
+
+            LOG.info("Polling GPS daemon")
+            try:
+                # Read messages to get the latest TPV and SKY
+                self._debug("Reading messages from GPS daemon")
+                for message in self.client.dict_stream(convert_datetime=True):
+                    msg_class = message.get("class")
+                    self._debug(f"Message class: {msg_class}")
+
+                    if msg_class == "TPV":
+                        tpv_data = message
+                    elif msg_class == "SKY":
+                        sky_data = message
+                    self._debug(f"TPV data: {tpv_data}")
+                    self._debug(f"SKY data: {sky_data}")
+
+                    # Once we have TPV data, process and break
+                    if tpv_data:
+                        break
+
+                if not tpv_data:
+                    LOG.warning("No GPS data available (no fix)")
+                    time.sleep(1)
+                    return True
+
+                # Check if we should beacon based on smart beaconing logic
+                if CONF.enable_smart_beacon:
+                    should_beacon, distance_feet = beacon_processor.should_beacon(
+                        tpv_data
+                    )
+                    if should_beacon:
+                        LOG.info("Sending beacon, enough movement or time has passed")
+                        self.send_beacon(tpv_data, sky_data)
+                    else:
+                        LOG.info(
+                            "Not sending beacon, not enough movement or time has passed"
+                        )
+                        time.sleep(1)
+                        return True
+                else:
+                    # Now only beacon if the time interval from CONF.beacon_interval has passed
+                    if time.time() - self.last_beacon_time < CONF.beacon_interval:
+                        LOG.info("Not sending beacon, time interval has not passed")
+                        time.sleep(1)
+                        return True
+                    else:
+                        self.send_beacon(tpv_data, sky_data)
+
+            except Exception as e:
+                LOG.error(f"Error polling GPS daemon: {e}")
+                time.sleep(1)
+                return True
+
+        time.sleep(1)
+        return True
